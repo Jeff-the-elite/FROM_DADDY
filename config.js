a2a2bab9961d3e5b7c40c831902ae61135c0e55e
Jeff-the-elite <150056211+Jeff-the-elite@users.noreply.github.com>
2026-02-12 22:26:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://collection.cloudinary.com/dhq366ptf/480a94997218f11a8cdf810fc5a1cfac", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dhq366ptf/video/upload/v1770906383/Taroko_e1459w.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 1.0                        // Volume level (0.0 to 1.0)
